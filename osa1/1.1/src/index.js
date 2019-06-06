d9a82f5db81f498251496fb43c975d652c14f742
Arttu Koponen <arttu.e.koponen@aalto.fi>
2019-06-06 23:24:34 +0300

    1.1 valmis

--- osa1/1.1/src/index.js
+++ osa1/1.1/src/index.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const App = () => (
+const Header = (props) => (
   <div>
-    <p>Hello world</p>
+    <h1>{props.header}</h1>
   </div>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Content = (props) => (
+  <div>
+    <p>{props.content1} {props.amount1}</p>
+    <p>{props.content2} {props.amount2}</p>
+    <p>{props.content3} {props.amount3}</p>
+  </div>
+)
+
+const Total = (props) => (
+  <div>
+    <p>{props.total}</p>
+  </div>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header header = {course}/>
+      <Content content1 = {part1} content2 = {part2} content3 = {part3} amount1 = {exercises1} amount2 = {exercises2} amount3 = {exercises3}/>
+      <Total total = {exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
