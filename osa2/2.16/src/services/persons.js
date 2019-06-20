956ef1ac3dd87a0204bcf92e912dfc23f828da30
Arttu Koponen <arttu.e.koponen@aalto.fi>
2019-06-20 14:48:59 +0300

    2.16 valmis

--- osa2/2.16/src/services/persons.js
+++ osa2/2.16/src/services/persons.js
@@ -13,9 +13,6 @@ const create = newObject => {
   return request.then(response => response.data)
 }
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
 
-export default {getAll, create, update}
+
+export default {getAll, create}
