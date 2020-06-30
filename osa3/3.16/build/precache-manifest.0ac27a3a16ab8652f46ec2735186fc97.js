e6c1453fe1aa9f3aff353d52ce5c883187e87d46
arnold1000 <you@example.com>
2020-06-30 22:12:15 +0200

    3.16 valmis

---
+++ osa3/3.16/build/precache-manifest.0ac27a3a16ab8652f46ec2735186fc97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "708332756d98a78a300d0a81733cb003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ddf97a48b4392f5d5d6",
+    "url": "/static/css/main.7f189203.chunk.css"
+  },
+  {
+    "revision": "6a10eb284c123e8ed17a",
+    "url": "/static/js/2.c9bd9752.chunk.js"
+  },
+  {
+    "revision": "5ddf97a48b4392f5d5d6",
+    "url": "/static/js/main.efc119e4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
