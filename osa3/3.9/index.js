09193388d07bdaff024b0fcd0300e0fec81e9dd3
arnold1000 <you@example.com>
2020-06-30 20:50:24 +0200

    changes

--- osa3/3.9/index.js
+++ osa3/3.9/index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   {
