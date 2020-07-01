c74c751c4429ad75b7ed84c78bb7a59d938a2290
arnold1000 <you@example.com>
2020-07-01 13:46:55 +0200

    3.21 valmis

---
+++ osa3/3.21/build/precache-manifest.3a3c2ae4b14002683c6837a29609557f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7da81f5345e389259f3a2c89a796294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "182e0061db9989c33b38",
+    "url": "/static/css/main.7f189203.chunk.css"
+  },
+  {
+    "revision": "6a10eb284c123e8ed17a",
+    "url": "/static/js/2.c9bd9752.chunk.js"
+  },
+  {
+    "revision": "182e0061db9989c33b38",
+    "url": "/static/js/main.a9d5c0bc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
