e602ed9c658e0433717f32ef8ff333f2a8398fb5
arnold1000 <you@example.com>
2020-07-02 15:49:07 +0200

    4.6 valmis

---
+++ osa4/4.6/utils/list_helper.js
@@ -0,0 +1,31 @@
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+  return 1
+}
+
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, cur) => acc + cur.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const hold = blogs.reduce((acc, cur) => (acc.likes > cur.likes) ? acc : cur)
+  return { title: hold.title, author: hold.author, likes: hold.likes }
+}
+
+const mostBlogs = (blogs) => {
+  const names = blogs.map(x => x.author)
+  console.log(names)
+  const counts = _(names).countBy().entries().maxBy(_.last)
+
+  return { author: counts[0], blogs: counts[1] }
+}
+
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
+}
